4429f6b8106a4eb292e9d237aa307402ee65acbf
pku2100012945 <2100012945@stu.pku.edu.cn>
2024-06-22 21:57:58 +0800

    Improving Error Handling

--- src/editor.rs
+++ src/editor.rs
@@ -7,25 +7,28 @@ impl Editor {
     pub fn default() -> Self {
         Editor {}
     }
+    // 调用实现函数，wrap 错误
     pub fn run(&self) {
-        enable_raw_mode().unwrap();
+        if let Err(err)=self.repl() {
+            panic!("{err:#?}");
+        }
+        println!("Goodbye.\r\n");
+    }
+    // 实现函数，可以传播错误
+    fn repl(&self)->Result<(),std::io::Error> {
+        // 错误传播
+        enable_raw_mode()?;
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{event:?} \r");
-                    match event.code {
-                        Char(c) => {
-                            if c == 'q' {
-                                break;
-                            }
-                        }
-                        _ => (),
+            if let Key(event)=read()? {
+                println!("{event:?} \r");
+                if let Char(c)=event.code {
+                    if c=='q' {
+                        break;
                     }
-                },
-                Err(err) => println!("Error: {err}"),
-                _ => (),
+                }
             }
         }
-        disable_raw_mode().unwrap();
+        disable_raw_mode()?;
+        Ok(())
     }
 }
